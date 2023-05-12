5f06d42eae0abfa37690099562adce28bc770080
mattiabenelli <benellimattia0@gmail.com>
2023-05-12 12:58:30 +0200

    es random num done

---
+++ src/org/java/myset/MainSet.java
@@ -0,0 +1,30 @@
+package org.java.myset;
+
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
+
+public class MainSet {
+
+	public static void main(String[] args) {
+		
+		Set<Integer> NumberList = new HashSet<>();
+		
+		Random r = new Random();
+		
+		
+		while(NumberList.size() < 5) {
+			
+			int randomNumber = r.nextInt(2, 13);
+			
+			if(!NumberList.contains(randomNumber)) {
+				
+				NumberList.add(randomNumber);
+			}
+		}
+		System.out.println(NumberList);
+	}
+	
+	
+}
